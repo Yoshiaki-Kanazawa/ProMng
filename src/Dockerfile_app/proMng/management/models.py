ad5ac42257f9c537d149b21b51e0cc3c0279464e
Yoshiaki-Kanazawa <k.yoshiaki1226@gmail.com>
2020-04-30 14:19:42 +0900

    model追加

--- src/Dockerfile_app/proMng/management/models.py
+++ src/Dockerfile_app/proMng/management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# 商品クラス
+class Product(models.Model):
+    name = models.CharField(min_length=1, max_length=50, unique=True)
+    amount = models.models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
