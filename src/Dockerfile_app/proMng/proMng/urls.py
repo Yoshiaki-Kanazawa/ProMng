971f6e006c3dfb07010e2e583622889bbc5b8ae4
Yoshiaki-Kanazawa <k.yoshiaki1226@gmail.com>
2020-05-01 11:49:03 +0900

    update

--- src/Dockerfile_app/proMng/proMng/urls.py
+++ src/Dockerfile_app/proMng/proMng/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 
-from Dockerfile_app.proMng.management.urls import router as management_router
+from management.urls import router as management_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
